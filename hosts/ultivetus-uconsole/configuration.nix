e6f911260752571d5efa18411140953bd7288aa0
Ultivetus <ultivetus@protonmail.com>
2026-05-24 17:49:36 +0900

    Update configuration.nix

--- hosts/ultivetus-uconsole/configuration.nix
+++ hosts/ultivetus-uconsole/configuration.nix
@@ -3,7 +3,11 @@
   imports = [
     ./hardware-configuration.nix
   ];
-
+  boot.loader.grub.enable = true;
+  boot.initrd.availableKernelModules = [ "nvme" "pcie-brcmstb" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  boot.kernelParams = [ "pciex1" ];
+  boot.loader.grub.device = "/dev/nvme0n1";
   networking.hostName = "ultivetus-uconsole";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "23.11";
